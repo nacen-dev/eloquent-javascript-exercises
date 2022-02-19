f2e3562d6d6fb8db9a7809906fa1d418fdd3c60d
nacen.dev <nacen.dev@gmail.com>
2022-02-19 17:40:06 +0800

    Complete fizzbuzz exercise

---
+++ 02-program-structure/fizzbuzz/fizzbuzz.js
@@ -0,0 +1,20 @@
+// My solution
+for (let num = 1; num <= 100; num++) {
+  if (num % 3 === 0 && num % 5 === 0) {
+    console.log("FizzBuzz");
+  } else if (num % 3 === 0) {
+    console.log("Fizz");
+  } else if (num % 5 === 0) {
+    console.log("Buzz");
+  } else {
+    console.log(num);
+  }
+}
+
+// Eloquent Javascript's Solution
+for (let n = 1; n <= 100; n++) {
+  let output = "";
+  if (n % 3 == 0) output += "Fizz";
+  if (n % 5 == 0) output += "Buzz";
+  console.log(output || n);
+}
