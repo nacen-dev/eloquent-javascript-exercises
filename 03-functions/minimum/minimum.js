916f97a33c0f9d4a2e5ba186190620a259ea4c1d
nacen.dev <nacen.dev@gmail.com>
2022-02-21 16:23:38 +0800

    Complete minimum exercise

---
+++ 03-functions/minimum/minimum.js
@@ -0,0 +1,16 @@
+// My solution
+function min(num1, num2) {
+  return num1 < num2 ? num1: num2;
+}
+
+
+// Eloquent Javascript's Solution
+
+// function min(a, b) {
+//   if (a < b) return a;
+//   else return b;
+// }
+
+
+console.log(min(0, 10));
+console.log(min(0, -10));
